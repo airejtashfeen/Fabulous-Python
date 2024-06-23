941d2ad779a3eb7e760a2d6d1aaafef329008347
Airej Tashfeen <airejtashfeen@gmail.com>
2024-06-24 04:23:19 +0500

    new file:   datavisualization.py

---
+++ datavisualization.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+
+plt.plot(x, y, marker='o')
+
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+plt.title('Graph check bas bro')
+
+plt.show()
